f5b8585797437548c61a58383b919c715d34d0d6
Eetu Häivälä <eetu.haivala@gmail.com>
2025-11-12 22:03:02 +0200

    Implement basic FileInput component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import { useState } from 'react';
+
+import { FileInput } from './components/FileInput/FileInput';
+
 function App() {
-  return <div className="App">Holvimankeli</div>;
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const handleFileSelect = (file: File): void => {
+    setSelectedFile(file);
+  };
+
+  const handleError = (error: Error): void => {
+    console.error('File selection error:', error);
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Holvimankeli</h1>
+      </header>
+      <main>
+        <FileInput
+          label="Select file"
+          acceptedFileTypes={['.xlsx']}
+          onFileSelect={handleFileSelect}
+          onError={handleError}
+        />
+        {selectedFile !== null && <p>Selected file: {selectedFile.name}</p>}
+      </main>
+    </div>
+  );
 }
 
 export default App;
